5e92a8ea02155438ce3562e80407a3bbc5bc38ac
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-01 21:45:43 -0400

    API setup completed

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,5 +2,7 @@
 {
     public class AppUser
     {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty; 
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
@@ -8,6 +11,10 @@ namespace API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
