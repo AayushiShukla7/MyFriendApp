fac6b35aeb958b9d4516c5232256aa7cfa5f6980
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-09 21:33:54 -0400

    Repository Pattern Implemented
    For centralization of the DataContext methods that this app supports.

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AppUser> GetUserById_Async(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByName_Async(string username)
+        {
+            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsers_Async()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;       // '> 0' check => Execute only if there are changes to make (**more than 0 rows affected)
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;  // Adds a flag to the entity that it has been modified
+        }
+    }
+}
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsers_Async();
+        Task<AppUser> GetUserById_Async(int id);
+        Task<AppUser> GetUserByName_Async(string username);
+    }
+}
