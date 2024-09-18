73f4d5e0be0ce7bd8ef0639bce55aa776d5da63d
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-18 17:28:25 -0400

    Like Entity added to DB

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,26 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.LikedUserId });
+
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -20,10 +20,7 @@ namespace API.Entities
         public string Country { get; set; } = string.Empty;
         public ICollection<Photo> Photos { get; set; }
 
-        // Extension Method -The name is very important (Must be - "Get<PropertyName>")
-        //public int GetAge()
-        //{
-        //    return DateOfBirth.CalculateAge();
-        //}
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
