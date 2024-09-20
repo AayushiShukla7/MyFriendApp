8bb6d3b168aa9938dd59c93b7fb06acd5541681f
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-20 18:04:59 -0400

    Setting up the entities for messaging in Asp.Net Core

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,11 +11,13 @@ namespace API.Data
 
         public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            // Likes
             modelBuilder.Entity<UserLike>()
                 .HasKey(k => new { k.SourceUserId, k.LikedUserId });
 
@@ -30,6 +32,18 @@ namespace API.Data
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.LikedUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            // Messages
+            modelBuilder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
         }
     }
 }
---
+++ API/Entities/Message.cs
@@ -0,0 +1,24 @@
+﻿namespace API.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        // Sender
+        public int SenderId { get; set; }
+        public string SenderUsername { get; set; }
+        public AppUser Sender { get; set; }
+
+        // Recipient
+        public int RecipientId { get; set; }
+        public string RecipientUsername { get; set; }
+        public AppUser Recipient { get; set; }
+
+        // Message-specific properties
+        public string Content { get; set; }
+        public DateTime? DateRead { get; set; }
+        public DateTime MessageSent { get; set; } = DateTime.Now;
+        public bool SenderDeleted { get; set; }
+        public bool RecipientDeleted { get; set; }
+    }
+}
