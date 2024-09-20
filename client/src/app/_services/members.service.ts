7b997d6493086110dd167aa28d5d4b797a3045e6
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-20 16:58:10 -0400

    Likes page updated with info

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -113,7 +113,7 @@ export class MembersService {
   }
 
   getLikes(predicate: string) {
-    return this.http.get(this.baseUrl + 'likes?=' + predicate);
+    return this.http.get<Partial<Member[]>>(this.baseUrl + 'likes?predicate=' + predicate);
   }
 
   // Generic Pagination
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MemberCardComponent } from '../members/member-card/member-card.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MemberCardComponent,
+    PaginationModule,
+    ButtonsModule
+  ],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
 export class ListsComponent {
+
+  members: Partial<Member>[];
+  predicate = 'liked';
+
+  constructor(private memberService: MembersService) {}
+
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe(response => {
+      this.members = response;
+    })
+  }
 
 }
