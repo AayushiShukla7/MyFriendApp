cf3e612c83388ff9a2cbe25037c5eaa6b4bc3b0d
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-25 17:12:09 -0400

    Adding policy based authorisation in Asp.Net Core

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("users-with-roles")]
+        public ActionResult GetUsersWithRoles()
+        {
+            return Ok("Only Admins can see this");
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photos-to-moderate")]
+        public ActionResult GetPhotosForModeration()
+        {
+            return Ok("Admins or Moderators can see this");
+        }
+    }
+}
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -35,6 +35,13 @@ namespace API.Extensions
                     };
                 });
 
+            // Configure Authorization [Policy-based]
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
             return services;
         }
     }
