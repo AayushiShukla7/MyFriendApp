5159373aad46192aed49a9fde74c4f0c82032479
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-25 21:00:55 -0400

    Editing roles in the client in Asp.Net Core

--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { User } from '../../_models/user';
 
 @Component({
   selector: 'app-roles-modal',
@@ -13,12 +14,21 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   templateUrl: './roles-modal.component.html',
   styleUrl: './roles-modal.component.css'
 })
-export class RolesModalComponent {
+export class RolesModalComponent implements OnInit {
 
-  title: string;
-  list: any[] = [];
-  closeBtnName: string;
+  @Input() updateSelectedRoles = new EventEmitter();
+  user: User;
+  roles: any[];
 
   constructor(public bsModalRef: BsModalRef) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  updateRoles() {
+    this.updateSelectedRoles.emit(this.roles);
+    this.bsModalRef.hide();
+  }
 
 }
