70b81d8cae17fe7a97d9af3d4a20b50986796565
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-26 13:06:43 -0400

    Member message read/unread indicator added

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -3,13 +3,15 @@ import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TimeagoModule } from 'ngx-timeago';
 
 @Component({
   selector: 'app-member-messages',
   standalone: true,
   imports: [
     FormsModule,
-    CommonModule
+    CommonModule,
+    TimeagoModule
   ],
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css'
