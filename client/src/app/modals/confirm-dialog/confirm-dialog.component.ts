9c5a0666e0496cf2c69b8fab60893d6530c0380b
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-09-27 20:05:16 -0400

    Adding a confirmation service to the angular app in Asp.Net Core

---
+++ client/src/app/modals/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.css'
+})
+export class ConfirmDialogComponent {
+
+  title: string;
+  message: string;
+  btnOkText: string;
+  btnCancelText: string;
+  result: boolean;
+
+  constructor(public bsModalRef: BsModalRef) {}
+
+  confirm() {
+    this.result = true;
+    this.bsModalRef.hide();
+  }
+
+  decline() {
+    this.result = false;
+    this.bsModalRef.hide();
+  }
+
+}
